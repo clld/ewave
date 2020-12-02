184fdf6bb0a8c6ceec3923df41034e25ebbfa45d
xrotwang <xrotwang@googlemail.com>
2020-12-02 16:03:30 +0100

    fixed dataset repos

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'clld>=7.1',
+        'clld>=7.4',
         'clldmpg>=4',
         'sqlalchemy',
         'waitress',
